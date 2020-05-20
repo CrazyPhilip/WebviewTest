02155f51e0df8909816efc567d74e0816a8eddeb
飞 程 <cf20090901@outlook.com>
2020-05-20 14:43:40 +0800

    添加项目文件。

---
+++ WebviewTest/WebviewTest.Android/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using CarouselView.FormsPlugin.Android;
+using Xamarin.Essentials;
+
+namespace WebviewTest.Droid
+{
+    [Activity(Label = "WebviewTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            //double systemDensity = DeviceDisplay.MainDisplayInfo.Density;
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+            var displayMetrics = Resources.DisplayMetrics;
+
+            Resources.DisplayMetrics.ScaledDensity = 2;//告诉android不要把自己大小单位缩放
+            Resources.DisplayMetrics.Density = 2;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            CarouselViewRenderer.Init();
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ WebviewTest/WebviewTest/NewPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Android.Util;
+
+namespace WebviewTest
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewPage : ContentPage
+    {
+        public NewPage()
+        {
+            InitializeComponent();
+
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+            var displayMetrics = Android.Content.Res.Resources.System.DisplayMetrics;
+
+        }
+    }
+}
